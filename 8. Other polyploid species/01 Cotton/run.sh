2f7d89c9475f9e694326f850814f12f01f70b1f6
Zhang Yixing <zyxjh21@126.com>
2025-09-03 18:09:15 +0800

    Update run.sh

--- 8. Other polyploid species/01 Cotton/run.sh
+++ 8. Other polyploid species/01 Cotton/run.sh
@@ -58,37 +58,12 @@ RUST_LOG=info ./panacus histgrowth -t10 -l 1,2,1,1,1 -q 0,0,1,0.5,0.1 -S -a  -c 
 ./panacus-visualize.py pggb.histgrowth.${i}.tsv > pggb.histgrowth.${i}.pdf
 done
 
-### 05. Simulate reads and map them to line genome and graph pangenome
-for fq in *.chr.fa; do
-    base=$(basename "$fq" .chr.fa)
-	art_illumina  -ss HS25 -i ${fq} -p -l 150 -f 20 -m 400 -s 10 -na -o ${base}_20X && gzip ${base}_20X1.fq &&  gzip ${base}_20X2.fq
-done
 
-vg autoindex --threads 20 --workflow giraffe -r ZY10.fa -p cotton_pggb   -v  merged_rename.vcf.gz
-
-for i in ./output/*X1.fq.gz; do
-    base=$(basename "$fq1" X1.fq.gz)
-    fq1_local="${base}X1.fq.gz"
-    fq2_local="${base}X2.fq.gz"
-    vg giraffe -t 10 -p -Z cotton_pggb.giraffe.gbz -m cotton_pggb.min -d cotton_pggb.dist -f ${fq1_local} -f ${fq2_local} 1>${base}.gam 2>${base}.log
-done
-
-
-bwa index ZY10.fa
-input_dir=./pres/
-output_dir=./
-for fq1 in $input_dir/*X1.fq; do
-    [ ! -f "$fq1" ] && continue
-    base=$(basename "$fq1" X1.fq)
-    fq2="${input_dir}/${base}X2.fq"
-    fq1_path="${input_dir}/${base}X1.fq"
-    sam_out="${output_dir}/${base}.bam"
-    log_out="${output_dir}/${base}.log"
-    bwa mem -t 40 ZY10.fa \"$fq1_path\" \"$fq2\" | samtools view -b > \"$sam_out\" 2> \"$log_out\"
-done
-
-### 06. Rna-seq analyse based on graph pangenome
-vg autoindex --threads 24 --workflow mpmap --workflow rpvg --prefix catton  --ref-fasta ZY10.chr.fa  -v merged_rename.vcf.gz \
+### 05. Rna-seq analyse based on graph pangenome
+# romove complex variants
+bcftools view -i 'strlen(REF)<1000 && strlen(ALT)<1000'  merged_rename.vcf.gz    -o merged_rename.filtered.vcf
+bgzip -@ 10 merged_rename.filtered.vcf
+vg autoindex --threads 24 --workflow mpmap --workflow rpvg --prefix catton  --ref-fasta ZY10.chr.fa  -v merged_rename.filtered.vcf.gz \
 --tx-gff ZY10.chr.gene.gff3 -T ./ --gff-tx-tag Parent
 GRAPH="../cotton.spliced.xg"
 GCSA="../cotton.spliced.gcsa"
